9eb7a04549370717238416236bef13d8bd30a9d4
Aad1tyakrkarn <106768201+Aad1tyakrkarn@users.noreply.github.com>
2024-03-06 17:24:07 +0530

    Update aqi.js

--- components/aqi.js
+++ components/aqi.js
@@ -1,1 +1,11 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, StyleSheet, Modal, ScrollView, FlatList, Image, SectionList, Pressable, Button, TouchableHighlight, TouchableOpacity, ActivityIndicator, StatusBar, Platform, TextInput } from "react-native";
 
+
+export const Aqi =(props)=>{
+    return(
+        <View>        
+            <Text style={{ color: "white",fontSize:27,marginBottom:15 }}>😷  {props.aqi}</Text>
+        </View>
+    )
+}
